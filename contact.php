684c6d733bd598d5a3f9fcec33d39e45a4062be8
C4PLANNING <awayan@c4planning.co.jp>
2022-07-11 17:45:23 +0900

    first commit

---
+++ contact.php
@@ -0,0 +1,183 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>株式会社ファーアウト−お仕事のご依頼はこちらから</title>
+    <link rel="icon" href="img/farout_logo_fav.png" type="image/x-icon">
+
+    <!-- jQuery -->
+    <script src="js/jquery-2.1.1.js"></script>
+
+    <!-- LocalStorage -->
+    <script src="js/garlic.js"></script>
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="ionicons/css/ionicons.min.css" rel="stylesheet">
+
+    <!-- main css -->
+    <link href="css/style.css" rel="stylesheet">
+
+    <!-- ▼ Lisket メールフォーム簡単作成ツール http://app.lisket.jp/form_maker ここから ▼ -->
+    <script type="text/javascript" src="//app.lisket.jp/bundles/lisketformmaker/js/form.js" charset="utf-8"></script>
+    <link rel="stylesheet" type="text/css" href="//app.lisket.jp/bundles/lisketformmaker/css/form.css" />
+    
+    <!-- modernizr -->
+    <script src="js/modernizr.js"></script>
+    
+    <link rel="stylesheet" type="text/css" href="css/lisket.css" />
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+
+<body>
+
+    <!-- Preloader -->
+    <div id="preloader">
+        <div class="pre-container">
+            <div class="spinner">
+                <div class="double-bounce1"></div>
+                <div class="double-bounce2"></div>
+            </div>
+        </div>
+    </div>
+    <!-- end Preloader -->
+
+    <div class="container-fluid">
+        <!-- box-header -->
+        <header class="box-header">
+            <div class="box-logo">
+                <a href="index.html"><img src="img/farout_logo.png" width="200" alt="Logo"></a>
+            </div>
+            <!-- box-nav -->
+            <a class="box-primary-nav-trigger" href="#0">
+                <span class="box-menu-text">Menu</span><span class="box-menu-icon"></span>
+            </a>
+            <!-- box-primary-nav-trigger -->
+        </header>
+        <!-- end box-header -->
+
+        <!-- nav -->
+        <nav>
+            <ul class="box-primary-nav">
+                <li><a href="index.html">Top</a></li>
+                <li><a href="about.html">About</a></li>
+                <li><a href="work.html">Business Showcase</a></li>
+                <li><a href="company.html">Company</a></li>
+                <li><a href="#">Contact</a> <i class="ion-ios-circle-filled color"></i> </li>
+                <li><a href="recruit.php">Recruit</a></li>  
+                <li><a href="access.html">Access</a></li>
+            </ul>
+        </nav>
+        <!-- end nav -->
+
+    </div>
+
+    <!-- top bar -->
+    <div class="top-bar" style="background: -webkit-linear-gradient( rgba(255, 255, 255, .7), rgba(255, 255, 255, .7)), url(img/bg17.jpg); background: linear-gradient( rgba(255, 255, 255, .7), rgba(255, 255, 255, .7)), url(img/bg17.jpg);     background-size: cover;"> 
+        <h1>contact</h1>
+        <p>お問い合わせはこちらから</p>
+    </div>
+    <!-- end top bar -->
+
+    <!-- main-container -->
+    <div class="container main-container">
+        <div class="col-md-12">
+            <h2>お問い合わせフォーム</h2>
+            <form class="lisket-form-maker-form" action="//app.lisket.jp/form_maker/message/12606" method="post" data-persist="garlic">
+
+                <table class="table table-striped">
+                    <tr>
+                        <th class="sp_border_none"><label for="data_name" class="required">お名前</label><span>(必須)</span></th><td><input type="text" id="data_name" name="data[name]" required="required" /><span class="example"></span></td>
+                    </tr>
+                    <tr>
+                        <th class="sp_border_none"><label for="data_field_1">会社名</label></th><td><input type="text" id="data_field_1" name="data[field_1]" /><span class="example"></span></td>
+                    </tr>
+                    <tr>
+                        <th class="sp_border_none"><label for="data_mail" class="required">メールアドレス</label><span>(必須)</span></th><td><input type="email" id="data_mail" name="data[mail]" required="required" /><span class="example"></span></td>
+                    </tr>
+                    <tr>
+                        <th class="sp_border_none"><label for="data_field_3" class="required">件名</label><span>(必須)</span></th><td><input type="text" id="data_field_3" name="data[field_3]" required="required" /><span class="example"></span></td>
+                    </tr>
+                    <tr>
+                        <th class="sp_border_none"><label for="data_field_4" class="required">ご質問内容</label><span>(必須)</span></th><td><textarea id="data_field_4" name="data[field_4]" required="required"></textarea><span class="example"></span></td>
+                    </tr>
+                    <tr style="display: none">
+                        <td colspan="2"><input type="hidden" id="data_verification_code" name="data[verification_code]" value="873662e1e7349ffee1439e2cdce91ff8ab4fd721" /></td>
+                    </tr>
+                    <tr>
+                        <td colspan="2" align="center" class="td-last-child"><input type="submit" value="送信" /><input type="reset" value="リセット" /></td>
+                    </tr>
+                </table>
+            </form>
+        </div>
+        <div class="col-md-12">
+            <div class="h-30"></div>
+            <p><a href="privacy.html">プライバシーポリシー</a>をご確認の上、送信をお願いいたします。</p>
+            <div class="contact-info">
+                <p><i class="ion-android-call"></i> 03-3356-0736</p>
+            </div>
+        </div>
+    </div>
+    <!-- end main-container -->
+
+    <!-- footer -->
+    <footer id="footer">
+        <div class="container">
+            <p class="company_group">グループ会社</p>
+            <div class="articles">
+                <!-- Icons -->
+                <article class="logos" style="background:#f5f6f7;">
+
+                    <div class="icons">
+                        <a href="https://c4planning.co.jp"><img src="img/c4_banner.jpg" class="banner"
+                                alt="企画、演出、制作という手法で「事づくり」をプロデュースする集団です"></a>
+                    </div>
+                    <br>
+
+                </article>
+                <article class="logos" style="background:#f5f6f7;">
+                    <div class="icons">
+                        <a href="https://no-x.co.jp"><img src="img/no-x_banner.jpg" class="banner"
+                                alt="各種イベント・展示会やキャンペーンでのコンパニオン・ナレーター・MC・モデル・スタッフのキャスティング"></a>
+                    </div>
+                    <br>
+                </article>
+                <article style="flex-basis:33%;">
+                </article>
+                <!-- copyright -->
+
+
+            </div>
+            <p class="copyright">© far out inc. All rights reserved.</p>
+        </div>
+    </footer>
+    <!-- end footer -->
+
+    <!-- back to top -->
+    <a href="#0" class="cd-top"><i class="ion-android-arrow-up"></i></a>
+    <!-- end back to top -->
+
+
+
+    <!--  plugins -->
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/menu.js"></script>
+    <script src="js/animated-headline.js"></script>
+    <script src="js/isotope.pkgd.min.js"></script>
+
+
+    <!--  custom script -->
+    <script src="js/custom.js"></script>
+
+</body>
+
+</html>
---
+++ recruit.php
@@ -0,0 +1,238 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>株式会社ファーアウト−採用情報</title>
+    <link rel="icon" href="img/farout_logo_fav.png" type="image/x-icon">
+
+
+    <!-- jQuery -->
+    <script src="js/jquery-2.1.1.js"></script>
+    <!-- LocalStorage -->
+    <script src="js/garlic.js"></script>
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="ionicons/css/ionicons.min.css" rel="stylesheet">
+
+    <!-- main css -->
+    <link href="css/style.css" rel="stylesheet">
+
+    <!-- ▼ Lisket メールフォーム簡単作成ツール http://app.lisket.jp/form_maker ここから ▼ -->
+    <script type="text/javascript" src="//app.lisket.jp/bundles/lisketformmaker/js/form.js" charset="utf-8"></script>
+    <link rel="stylesheet" type="text/css" href="//app.lisket.jp/bundles/lisketformmaker/css/form.css" />
+
+    <!-- modernizr -->
+    <script src="js/modernizr.js"></script>
+
+    <link rel="stylesheet" type="text/css" href="css/lisket.css" />
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+
+<body>
+
+    <!-- Preloader -->
+    <div id="preloader">
+        <div class="pre-container">
+            <div class="spinner">
+                <div class="double-bounce1"></div>
+                <div class="double-bounce2"></div>
+            </div>
+        </div>
+    </div>
+    <!-- end Preloader -->
+
+    <div class="container-fluid">
+        <!-- box-header -->
+        <header class="box-header">
+            <div class="box-logo">
+                <a href="index.html"><img src="img/farout_logo.png" width="200" alt="Logo"></a>
+            </div>
+            <!-- box-nav -->
+            <a class="box-primary-nav-trigger" href="#0">
+                <span class="box-menu-text">Menu</span><span class="box-menu-icon"></span>
+            </a>
+            <!-- box-primary-nav-trigger -->
+        </header>
+        <!-- end box-header -->
+
+        <!-- nav -->
+        <nav>
+            <ul class="box-primary-nav">
+                <li><a href="index.html">Top</a></li>
+                <li><a href="about.html">About</a></li>
+                <li><a href="work.html">Business Showcase</a></li>
+                <li><a href="company.html">Company</a></li>
+                <li><a href="contact.php">Contact</a> </li>
+                <li><a href="recruit.php">Recruit</a>　<i class="ion-ios-circle-filled color"></i> </li>  
+                <li><a href="access.html">Access</a> </li>
+            </ul>
+        </nav>
+        <!-- end nav -->
+
+    </div>
+
+    <!-- top bar -->
+    <div class="top-bar" style="background: -webkit-linear-gradient( rgba(255, 255, 255, .7), rgba(255, 255, 255, .7)), url(img/bg17.jpg); background: linear-gradient( rgba(255, 255, 255, .7), rgba(255, 255, 255, .7)), url(img/bg17.jpg);     background-size: cover;">
+        <h1>recruit</h1>
+        <p>採用情報</p>
+    </div>
+    <!-- end top bar -->
+   
+    <div class="container">
+        <div class="service-box" style="margin-top: 40px;">
+            <p>ファーアウトでは、正社員の募集を行なっております。</p>
+            <br>
+            <p>音響関連の業務経験のあるなしによらずご応募いただけます。</p>
+            <p>継続して勤務いただける方を希望いたします。</p>
+            <table class="table table-striped" style="border-bottom:1px solid #ddd;">
+                <tbody>
+                    <tr>
+                        <th>職種</th>
+                        <td>PAエンジニア</td>
+                    </tr>
+                    <tr>
+                        <th>必要資格</th>
+                        <td>普通自動車運転免許（AT、MT）</td>
+                    </tr>
+                    <tr>
+                        <th>年齢</th>
+                        <td>18〜35才くらいまで（性別不問）</td>
+                    </tr>
+                    <tr>
+                        <th>募集形態</th>
+                        <td>正社員（試用期間あり）</td>
+                    </tr>
+                    <tr>
+                        <th>諸手当</th>
+                        <td>交通費支給<br>社会保険完備</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+    <!-- main-container -->
+    <div class="container main-container">  
+        <div class="col-md-12">
+            <h2>求人応募フォーム</h2>
+            <form class="lisket-form-maker-form" action="//app.lisket.jp/form_maker/message/12605" method="post" data-persist="garlic">
+
+                <table class="table table-striped">
+                    <tr>
+                        <th class="sp_border_none"><label for="data_name" class="required">お名前</label><span>(必須)</span></th>
+                        <td><input type="text" id="data_name" name="data[name]" required="required" /><span class="example"></span></td>
+                    </tr>
+                    <tr>
+                        <th class="sp_border_none"><label for="data_field_1" class="required">ふりがな</label><span>(必須)</span></th>
+                        <td><input type="text" id="data_field_1" name="data[field_1]" required="required" /><span class="example"></span></td>
+                    </tr>
+                    <tr>
+                        <th class="sp_border_none"><label for="data_mail" class="required">メールアドレス</label><span>(必須)</span></th>
+                        <td><input type="email" id="data_mail" name="data[mail]" required="required" /><span class="example"></span></td>
+                    </tr>
+                    <tr>
+                        <th class="sp_border_none"><label for="data_field_3">電話番号</label></th>
+                        <td><input type="text" id="data_field_3" name="data[field_3]" /><span class="example"></span></td>
+                    </tr>
+                    <tr>
+                        <th class="sp_border_none"><label class="required">性別</label><span>(必須)</span></th>
+                        <td>
+                            <div id="data_field_4"><input type="radio" id="data_field_4_0" name="data[field_4]" required="required" value="男性" /><label for="data_field_4_0" class="required">男性</label><input type="radio" id="data_field_4_1" name="data[field_4]" required="required" value="女性" /><label for="data_field_4_1" class="required">女性</label></div><span class="example"></span>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th class="sp_border_none"><label for="data_field_5" class="required">年齢</label><span>(必須)</span></th>
+                        <td><input type="text" id="data_field_5" name="data[field_5]" required="required" /><span class="example"></span></td>
+                    </tr>
+                    <tr>
+                        <th class="sp_border_none"><label for="data_field_6" class="required">職歴</label><span>(必須)</span></th>
+                        <td><textarea id="data_field_6" name="data[field_6]" required="required"></textarea><span class="example"></span></td>
+                    </tr>
+                    <tr>
+                        <th class="sp_border_none"><label class="required">個人情報について</label><span>(必須)</span></th>
+                        <td>
+                            <div id="data_field_7"><input type="radio" id="data_field_7_0" name="data[field_7]" required="required" value="" class="radio1" />
+                                <label for="data_field_7_0" class="radio">プライバシーポリシーに同意する</label></div>
+                            <span class="example"></span>
+                        </td>
+                    </tr>
+                    <tr style="display: none">
+                        <td colspan="2"><input type="hidden" id="data_verification_code" name="data[verification_code]" value="0b73f62e7f045aaebc3c50041c26b606e2f7bc06" /></td>
+                    </tr>
+                    <tr>
+                        <td colspan="2" align="center" class="td-last-child"><input type="submit" value="送信" /><input type="reset" value="リセット" /></td>
+                    </tr>
+                </table>
+            </form>
+        </div>
+        <div class="col-md-12">
+            <div class="h-30"></div>
+            <p><a href="privacy.html">プライバシーポリシー</a>をご確認の上、送信をお願いいたします。</p>
+            <div class="contact-info">
+                <p><i class="ion-android-call"></i> 03-3356-0736</p>
+            </div>
+        </div>
+    </div>
+    
+    <!-- end main-container -->
+
+    <!-- footer -->
+    <footer id="footer">
+        <div class="container">
+            <p class="company_group">グループ会社</p>
+            <div class="articles">
+                <!-- Icons -->
+                <article class="logos" style="background:#f5f6f7;">
+
+                    <div class="icons">
+                        <a href="https://c4planning.co.jp"><img src="img/c4_banner.jpg" class="banner"
+                                alt="企画、演出、制作という手法で「事づくり」をプロデュースする集団です"></a>
+                    </div>
+                    <br>
+
+                </article>
+                <article class="logos" style="background:#f5f6f7;">
+                    <div class="icons">
+                        <a href="https://no-x.co.jp"><img src="img/no-x_banner.jpg" class="banner"
+                                alt="各種イベント・展示会やキャンペーンでのコンパニオン・ナレーター・MC・モデル・スタッフのキャスティング"></a>
+                    </div>
+                    <br>
+                </article>
+                <article style="flex-basis:33%;">
+                </article>
+                <!-- copyright -->
+
+
+            </div>
+            <p class="copyright">© far out inc. All rights reserved.</p>
+        </div>
+    </footer>
+    <!-- end footer -->
+
+    <!-- back to top -->
+    <a href="#0" class="cd-top"><i class="ion-android-arrow-up"></i></a>
+    <!-- end back to top -->
+
+
+    <!--  plugins -->
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/menu.js"></script>
+    <script src="js/animated-headline.js"></script>
+    <script src="js/isotope.pkgd.min.js"></script>
+
+
+    <!--  custom script -->
+    <script src="js/custom.js"></script>
+
+</body>
+
+</html>
